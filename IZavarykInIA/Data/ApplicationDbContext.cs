4261ab82e20d6d02ad6062419646a787620c48db
ig0rz <is23zavarykinia@artcollege.ru>
2022-12-14 13:31:16 +0300

    Добавьте файлы проекта.

---
+++ IZavarykInIA/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using IZavarykInIA.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace IZavarykInIA.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
+
---
+++ IZavarykInIA/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IZavarykInIA.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Поле не может быть пустым")]
+        [DisplayName("Название")]
+        public string? Title { get; set; }
+        [DisplayName("Описание")]
+        public string Description { get; set; }
+        [DisplayName("Рейтинг")]
+        [Required(ErrorMessage="Поле не может быть пустыи")]
+        [Range(1,int.MaxValue,ErrorMessage="Значение должно быть больше нуля")]
+        public string Rating { get; set; }
+
+    }
+}
